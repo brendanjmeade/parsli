18f8c3589193f7635cabdf9d901ced5895fd518b
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2025-05-29 16:57:06 -0600

    feat(topo): implement topo readers

--- src/parsli/io/__init__.py
+++ src/parsli/io/__init__.py
@@ -2,9 +2,13 @@ from __future__ import annotations
 
 from .coast import VtkCoastLineSource
 from .mesh import VtkMeshReader
+from .rivers import RiverReader
 from .segment import VtkSegmentReader
+from .topo import TopoReader
 
 __all__ = [
+    "RiverReader",
+    "TopoReader",
     "VtkCoastLineSource",
     "VtkMeshReader",
     "VtkSegmentReader",
